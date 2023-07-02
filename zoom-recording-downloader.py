d92a505157a8874721998ca4eda8f5e4971b7c26
Jeff Santos <jmsapps@outlook.com>
2023-07-02 00:22:02 -0400

    Updated documentation

--- zoom-recording-downloader.py
+++ zoom-recording-downloader.py
@@ -33,11 +33,11 @@ ACCOUNT_ID = CONF["account_id"]
 CLIENT_ID = CONF["client_id"]
 CLIENT_SECRET = CONF["client_secret"]
 
-APP_VERSION = "2.1"
+APP_VERSION = "3.0 (OAuth)"
 API_ENDPOINT_USER_LIST = 'https://api.zoom.us/v2/users'
 
 # Start date now split into YEAR, MONTH, and DAY variables (Within 6 month range)
-RECORDING_START_YEAR = 2022
+RECORDING_START_YEAR = 2023
 RECORDING_START_MONTH = 1
 RECORDING_START_DAY = 1
 RECORDING_END_DATE = datetime.date.today()
